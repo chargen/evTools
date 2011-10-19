15888cd4403646caf1743d842cb98dd34ca96bdf
AstroFloyd <AstroFloyd@gmail.com>
2011-10-19 13:54:59 +0000

    Make sure the code compiles with g95

--- CMake/FileList.cmake
+++ CMake/FileList.cmake
@@ -1,8 +1,15 @@
+
 set(evTools_std_files
   src/code_version.f90
   src/functions.f90
-  #src/nagfor.f90  # Needed to compile with nagfor, g95, and F2003 standard - won't run!
   )
+
+# Get compiler name, add a file for g95:
+get_filename_component( Fortran_COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME )
+if( Fortran_COMPILER_NAME MATCHES "g95" )
+  set( evTools_std_files ${evTools_std_files} src/nagfor.f90 )  # Needed to compile with nagfor, g95, and F2003 standard - won't run!
+endif( Fortran_COMPILER_NAME MATCHES "g95" )
+
 
 set(evTools_plot_files
   src/plotfunctions.f90

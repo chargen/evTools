2ed57a53d1514e4a0e07bf3b465d501803b49b10
AstroFloyd <AstroFloyd@gmail.com>
2012-11-26 18:00:35 +0100

    Updating num_sp_type_2_lt()
    * Updating num_sp_type_2_lt():
      * implemented 40-term Chebyshev approximation
        * works if i,j are swapped hen using cij and dij
    
      * implemented example cheby40i.awk
        * uses the recurrence relation for Chebyshev polynomials of the first kind, rather
          than the trigonometric definition
        * works

--- src/testplt.f90
+++ src/testplt.f90
@@ -1,5 +1,5 @@
 !***********************************************************************************************************************************
-program selplt
+program testplt
   use kinds, only: double
   implicit none
   integer :: spi,lci
@@ -10,14 +10,15 @@ program selplt
   !write(*,'(2A6, 2A8)') 'lumcl','sptyp','logT','logL'
   
   if(1.eq.1) then
-     write(*,'(/,8x)', advance='no')
+     write(*,'(/,A)') 'log Teff:'
+     write(*,'(8x)', advance='no')
      do lci=0,5
         lumcl = dble(lci)
         write(*,'(F8.1)', advance='no') lumcl
      end do
      write(*,*)
      
-     do spi = 30,830,800 !40
+     do spi = 30,830,200 !800 !40
         sptyp = dble(spi)/100.d0
         lci = 5
         write(*,'(F8.2)', advance='no') sptyp
@@ -35,14 +36,15 @@ program selplt
      write(*,*)
   end if
   
-  write(*,'(/,8x)', advance='no')
+  write(*,'(/,A)') 'log L:'
+  write(*,'(8x)', advance='no')
   do lci=0,5
      lumcl = dble(lci)
      write(*,'(F8.1)', advance='no') lumcl
   end do
   write(*,*)
   
-  do spi = 30,790,760 !40
+  do spi = 30,790,190 !760 !40
      sptyp = dble(spi)/100.d0
      lci = 5
      write(*,'(F8.2)', advance='no') sptyp
@@ -58,6 +60,23 @@ program selplt
      write(*,*)
   end do
   write(*,*)
-
-end program selplt
+  
+  
+  ! Compare to cheby40i.awk
+  write(*,*)
+  sptyp = 0.3d0
+  lumcl = 1.d0
+  do spi=1,7
+     if(spi.eq.2) sptyp = 1.05d0
+     if(spi.eq.3) sptyp = 2.10d0
+     if(spi.eq.4) sptyp = 4.d0
+     if(spi.eq.5) sptyp = 5.d0
+     if(spi.eq.6) sptyp = 6.d0
+     if(spi.eq.7) sptyp = 6.9d0
+     call num_sp_type_2_lt(sptyp,lumcl, lum,teff)
+     write(*,'(2F6.2,5x,2F7.3)') sptyp,lumcl, log10((/teff,lum/))
+  end do
+  write(*,*)
+  
+end program testplt
 !***********************************************************************************************************************************
